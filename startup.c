7c623135079d11476e5905c5086216373841037e
TalesNeves <tales.neves@disroot.org>
2022-06-08 16:42:40 -0300

    makefile correction

--- startup.c
+++ startup.c
@@ -6,29 +6,34 @@
 #define STACK_START SRAM_END
 
 void reset_handler(void);
+void default_handler(void);
 void nmi_handler(void);
 
 uint32_t vectors[] __attribute__((section(".isr_vectors"))) = {
 
     STACK_START,                /* 0x0000 0000 */
     (uint32_t)reset_handler,    /* 0x0000 0004 */
-    0,                          /* 0x0000 0008 */
-    0,                          /* 0x0000 000c */
-    0,                          /* 0x0000 0010 */
-    0,                          /* 0x0000 0014 */
-    0,                          /* 0x0000 0018 */
-    0,                          /* 0x0000 001c */
+    (uint32_t)nmi_handler,      /* 0x0000 0008 */
+    (uint32_t)default_handler,  /* 0x0000 000c */
+    (uint32_t)default_handler,  /* 0x0000 0010 */
+    (uint32_t)default_handler,  /* 0x0000 0014 */
+    (uint32_t)default_handler,  /* 0x0000 0018 */
+    0,  /* 0x0000 001c */
     0,                          /* 0x0000 0020 */
     0,                          /* 0x0000 0024 */
     0,                          /* 0x0000 0028 */
-    0,                          /* 0x0000 002c */
-    0,                          /* 0x0000 0030 */
+    (uint32_t)default_handler,  /* 0x0000 002c */
+    (uint32_t)default_handler,  /* 0x0000 0030 */
     0,                          /* 0x0000 0034 */
-    0,                          /* 0x0000 0038 */
-    0,                          /* 0x0000 003c */
+    (uint32_t)default_handler,  /* 0x0000 0038 */
+    (uint32_t)default_handler,  /* 0x0000 003c */
 };
 
 void reset_handler(void){
+
+}
+
+void default_handler(void){
 
 }
 
